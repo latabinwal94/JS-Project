78a0641f163edcb73f22e846e747ec94da444f58
Lata Binwal <lbinwal@dminc.com>
2021-09-27 18:13:10 +0530

    Initial commit for hex color code convertor

---
+++ app.js
@@ -0,0 +1,12 @@
+const colors = ['green', 'red', 'rgba(133,122,200)', '#f15025', 'rgba(0,0,0,0)']
+const btn = document.getElementById('btn')
+const color = document.querySelector('.color')
+btn.addEventListener('click', function() {
+    const randomNumber = getRandomNumber()
+    document.body.style.backgroundColor = colors[randomNumber]
+    color.textContent = colors[randomNumber]
+})
+
+const getRandomNumber = () => {
+    return Math.floor(Math.random() * colors.length)
+}
---
+++ hex.js
@@ -0,0 +1,15 @@
+const hexColor = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,'A', 'B', 'C', 'D', 'E', 'F']
+const btn = document.getElementById('btn')
+const color = document.querySelector('.color')
+btn.addEventListener('click', function() {
+    let hex = '#'
+    for(let i=0; i<6; i++) {
+        hex += hexColor[getRandomNumber()]
+    }
+    document.body.style.backgroundColor = hex
+    color.textContent = hex
+})
+
+const getRandomNumber = () => {
+    return Math.floor(Math.random() * hexColor.length)
+}
